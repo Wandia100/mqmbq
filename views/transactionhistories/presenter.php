b1ced9d8f20a4cd2e898ab59d8815c5150b20920
kutasimiyu <simiyukuta@gmail.com>
2021-04-07 12:36:22 +0300

    presenter dash

---
+++ views/transactionhistories/presenter.php
@@ -0,0 +1,19 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\TransactionHistoriesSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Presenter DashBoard';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="transaction-histories-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    presenter content comes here 
+
+</div>
