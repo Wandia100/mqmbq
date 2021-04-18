6553b3783f484fda0070382253c5eddde88804d5
kutasimiyu <simiyukuta@gmail.com>
2021-04-18 16:46:27 +0300

    resolved error on assigning of transactions

--- controllers/MpesapaymentsController.php
+++ controllers/MpesapaymentsController.php
@@ -127,7 +127,7 @@ class MpesapaymentsController extends Controller
             $model=new MpesaPayments();
             $model=$row;
             $model->id=Uuid::generate()->string;
-            $model->BillRefNumber="RAMOGI";
+            $model->BillRefNumber="CHAMGEI";
             $model->created_at=date("Y-m-d H:i:s");
             $model->updated_at=date("Y-m-d H:i:s");
             $model->state=0;
