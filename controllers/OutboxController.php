29b6ec73a742ff64e87ef371d369a0d84231d909
kutasimiyu <simiyukuta@gmail.com>
2021-04-25 21:35:43 +0300

    outbox and sentsms securing

--- controllers/OutboxController.php
+++ controllers/OutboxController.php
@@ -19,7 +19,23 @@ class OutboxController extends Controller
      */
     public function behaviors()
     {
+        
         return [
+            'access' => [
+                'class' => \yii\filters\AccessControl::className(),
+                'only' => ['create', 'update','index'],
+                'rules' => [
+                    [
+                        'actions' => ['create', 'update','index'],
+                        'allow' => true,
+                        'matchCallback' => function ($rule, $action) {
+                            if(!Yii::$app->user->isGuest){
+                                return TRUE;
+                            }
+                        }
+                    ],
+                ],
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
