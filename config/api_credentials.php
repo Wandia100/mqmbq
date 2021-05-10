accf1fd139935eebd26c77cbc7031b7fd8715a75
kutasimiyu <simiyukuta@gmail.com>
2021-05-10 23:21:36 +0300

    mpesa keys for coke and net update

--- config/api_credentials.php
+++ config/api_credentials.php
@@ -1,11 +1,38 @@
 <?php
-define( 'PARTYA', '3015585' );
-define( 'REMARKS', 'Remarks' );
-define( 'QUEUETIMEOUTURL',"https://comp21.co.ke/api/disbursement-payment-timeout-result" );
-define( 'RESULTURL',"https://comp21.co.ke/api/disbursement-payment-result-confirmation" );
-define( 'INITIATORNAME',"com21.api" );
-define( 'OCCASION',"Occasion");
-define( 'MPESAPAYMENTREQUESTURL',"https://api.safaricom.co.ke/mpesa/b2c/v1/paymentrequest");
-define( 'MPESATOKENURL',"https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials");
-define( 'NITEXTSMSURL',"https://nitext.co.ke/index.php/api/sendSmsMultiple");
+$server_name = $_SERVER['SERVER_NAME'];
+$comp21coke = array( '18.222.117.89','www.comp21.co.ke', 'comp21.co.ke');
+$comp21net = array( '18.190.157.46','www.comp21.net', 'comp21.net' );
+$comp21dev = array( '18.222.117.89','www.comp21.co.ke', 'comp21.co.ke');
+if ( ( in_array($server_name, $comp21coke))) {
+    define( 'PARTYA', '3015585' );
+    define( 'REMARKS', 'Remarks' );
+    define( 'QUEUETIMEOUTURL',"https://comp21.co.ke/api/disbursement-payment-timeout-result" );
+    define( 'RESULTURL',"https://comp21.co.ke/api/disbursement-payment-result-confirmation" );
+    define( 'INITIATORNAME',"com21.api" );
+    define( 'OCCASION',"Occasion");
+    define( 'MPESAPAYMENTREQUESTURL',"https://api.safaricom.co.ke/mpesa/b2c/v1/paymentrequest");
+    define( 'MPESATOKENURL',"https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials");
+    define( 'NITEXTSMSURL',"https://nitext.co.ke/index.php/api/sendSmsMultiple");
+}
+else if ( ( in_array($server_name,$comp21net))) {
+    define( 'PARTYA', '3015585' );
+    define( 'REMARKS', 'Remarks' );
+    +define( 'QUEUETIMEOUTURL',"https://comp21.net/api/disbursement-payment-timeout-result" );
+    +define( 'RESULTURL',"https://comp21.net/api/disbursement-payment-result-confirmation" );
+    define( 'INITIATORNAME',"com21.api" );
+    define( 'OCCASION',"Occasion");
+    define( 'MPESAPAYMENTREQUESTURL',"https://api.safaricom.co.ke/mpesa/b2c/v1/paymentrequest");
+    define( 'MPESATOKENURL',"https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials");
+    define( 'NITEXTSMSURL',"https://nitext.co.ke/index.php/api/sendSmsMultiple");
+}
+else if ( ( in_array($server_name,$comp21dev))) {
+    $db_host      = "3.141.4.51";
+}
+ else {
+    $db_host      = "127.0.0.1";
+    $db_username = "com2120";
+    $db_password = "Zpa@!%vfrG34f";
+    $db_name = "com21";
+ }
+
 ?>
