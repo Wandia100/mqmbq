74725ae1c4676911f2b4b9d5a653c952fe7d9dc1
Michael Kinuthia <mikikimwang@gmail.com>
2021-07-18 15:27:44 +0300

    Financial summary PDF: Total

--- views/report/partials/payouts_view.php
+++ views/report/partials/payouts_view.php
@@ -8,6 +8,7 @@
     </thead>
     <tbody>
     <?php
+    $total = 0;
     for($i=0;$i< count($response1); $i++)
     {
         ?>
@@ -16,8 +17,13 @@
         <td><?=$response1[$i]['totalamount'];?></td>
         </tr>
         <?php
+        $total = $total + $response1[$i]['totalamount'];
     }
         ?>
+        <tr>
+            <td><b>Total</b></td>
+            <td><b><?=$total?></b></td>
+        </tr>
     </tbody>
 </table>
 
@@ -34,6 +40,7 @@
     </thead>
     <tbody>
     <?php
+    $totalcom = 0;
     for($i=0;$i< count($response2); $i++)
     {
         ?>
@@ -44,7 +51,14 @@
         <td><?= isset($response2[$i]['totalamount'])?$response2[$i]['totalamount']:'';?></td>
         </tr>
         <?php
+        $totalcom = $totalcom + $response2[$i]['totalamount'];
     }
         ?>
+        <tr>
+            <td><b>TOTAL</b></td>
+            <td></td>
+            <td></td>
+            <td><b><?=$totalcom?></b></td> 
+        </tr>
     </tbody>
 </table>
