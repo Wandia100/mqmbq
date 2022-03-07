c76837fd780992ddd3c106b76f1c0278352ed4ab
kuta <simiyukuta@gmail.com>
2022-03-07 18:38:06 +0300

    site controller

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -112,12 +112,6 @@ class SiteController extends Controller
 
         $model = new LoginForm();
         if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            $session = Yii::$app->session;
-            if(in_array(\Yii::$app->user->identity->perm_group, [4,5])){
-                $session->set('isstationmanager', true);
-            }else{
-                $session->set('isstationmanager', false);
-            }
             return $this->goBack();
         }
 
