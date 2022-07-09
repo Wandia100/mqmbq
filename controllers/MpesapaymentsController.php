b6e4945db0f12b724c0d45baaa5100f6241da33b
kuta <simiyukuta@gmail.com>
2022-07-09 17:31:02 +0300

    tigo implementation

--- controllers/MpesapaymentsController.php
+++ controllers/MpesapaymentsController.php
@@ -32,10 +32,10 @@ class MpesapaymentsController extends Controller
         return [
             'access' => [
                 'class' => \yii\filters\AccessControl::className(),
-                'only' => ['create', 'update','index','airtel','vodacom','correctairteldate'],
+                'only' => ['create', 'update','index','airtel','vodacom','correctairteldate','tigo'],
                 'rules' => [
                     [
-                        'actions' => ['create', 'update','index','airtel','vodacom','correctairteldate'],
+                        'actions' => ['create', 'update','index','airtel','vodacom','correctairteldate','tigo'],
                         'allow' => true,
                         'matchCallback' => function ($rule, $action) {
                             if ( ! Yii::$app->user->isGuest ) {
@@ -370,6 +370,63 @@ class MpesapaymentsController extends Controller
 		}
 
 		return $this->render( 'vodacom', [
+				'success' => $success,
+				'error'   => $error
+			]
+		);
+
+	}
+    public function actionTigo() {
+		ini_set( 'memory_limit', '512M' );
+		ini_set( 'max_execution_time', '3000' );
+		$success = [];
+		$error   = [];
+		if ( isset( $_POST['submit'] ) ) {
+			$file = $_FILES['file']['tmp_name'];
+            $reference=$_POST['reference'];
+//			$file    = "confirmed_payment.csv";
+			$success = [];
+			$error   = [];
+			$row     = 1;
+			if ( ( $handle = fopen( $file, "r" ) ) !== false ) {
+				while ( ( $data = fgetcsv( $handle, 2000, "," ) ) !== false ) {
+                    
+                                    $transaction_number=trim(isset($data[0])?$data[0]:NULL);
+                                    $phone=trim(isset($data[1])?$data[1]:NULL);
+                                    $reference="PESA";
+                                    $date=trim(isset($data[5])?$data[5]:NULL);
+                                    $time=trim(isset($data[6])?$data[6]:NULL);
+                                    $amount=trim(isset($data[4])?$data[4]:NULL);
+                                    $balance=0;
+					if (!empty($transaction_number) && !empty($reference)
+                    && !empty($date) && !empty($phone) && !empty($amount) && is_numeric($amount) && $amount==1000) {
+							$check_if_exists = MpesaPayments::find()->where( [ 'TransID' => $transaction_number ] )->one();
+							if ($check_if_exists == NULL) {
+								$mod= new MpesaPayments();
+								$mod->id=Uuid::generate()->string;
+                                $mod ->TransID = $transaction_number;
+                                $mod -> TransAmount = $amount;
+                                $mod -> FirstName = NULL; 
+                                $mod -> MiddleName = NULL; 
+                                $mod -> LastName = NULL; 
+                                $mod -> MSISDN = $phone; 
+                                $mod -> BillRefNumber = $reference;
+                                $mod -> OrgAccountBalance =$balance;
+                                $mod -> created_at = date("Y-m-d",strtotime($date))." ".date("H:i:s",strtotime($time));
+                                $mod -> updated_at = date('Y-m-d H:i:s');
+                                $mod ->save(FALSE);
+                                Yii::$app->queue->push(new DepositJob(['id'=>$mod->id]));
+								array_push( $success, $row );
+							}
+							array_push( $error, $row );
+					}
+					$row ++;
+				}
+				fclose( $handle );
+			}
+		}
+
+		return $this->render( 'tigo', [
 				'success' => $success,
 				'error'   => $error
 			]
