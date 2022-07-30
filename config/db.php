14b18c72fbc39d1aa8b08b9dcea057a02c8e706a
kuta <simiyukuta@gmail.com>
2022-07-30 17:01:26 +0300

    efmtz db

--- config/db.php
+++ config/db.php
@@ -37,7 +37,7 @@
       $db_name = "mchezo";
       }      
     else if ( ( in_array($host_name,[EFMTZ_COM]))) {
-      $db_host      = "db.efmtz.com";
+      $db_host      = "3.17.59.95";
       $db_username = "efmtz2120";
       $db_password = "Zpa@!%vfrG34f";
       $db_name = "efmtz";
